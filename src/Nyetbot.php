08266096e53d50daaebc4e22c521aba4de5b7f8b
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-11-16 23:24:17 +0700

    Add basic setter and getter. Gonna add HTTP request soon!

--- src/Nyetbot.php
+++ src/Nyetbot.php
@@ -4,8 +4,75 @@ namespace Nyetbot;
 
 class Nyetbot
 {
-	public function __construct()
+	protected $api;
+
+	protected $channelAccessToken;
+
+	protected $channelSecret;
+
+	public function __construct($channelAccessToken = null, $channelSecret = null)
 	{
-		echo "Kyaaa you see my first code >////<";
+		if($channelAccessToken != null)
+		{
+			$this->channelAccessToken = $channelAccessToken;
+		}
+
+		if($channelSecret != null)
+		{
+			$this->channelSecret = $channelSecret;
+		}
+	}
+
+	public function setChannelAcessToken($channelAccessToken): void
+	{
+		$this->channelAccessToken = $channelAccessToken;
+	}
+
+	public function getChannelAccessToken(): string
+	{
+		return $this->channelAccessToken;
+	}
+
+	public function setChannelSecret($channelSecret): void
+	{
+		$this->channelSecret = $channelSecret;
+	}
+
+	public function getChannelSecret(): string
+	{
+		return $this->channelSecret;
+	}
+
+	public function setApi($api): void
+	{
+		$this->api = $api;
+	}
+
+	public function getApi(): string
+	{
+		return $this->api;
+	}
+
+	public function get($args = null)
+	{
+		if($args == null) {
+			$ch = curl_init($this->api);
+		}
+		else {
+			$ch = curl_init(vsprintf($this->api, $args));
+		}
+		curl_setopt($ch, CURLOPT_HTTPGET, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Authorization: Bearer '.$this->channelAccessToken
+		));
+		
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+			error_log('Error:' . curl_error($ch));
+		}
+		curl_close($ch); 
+
+		return $result;
 	}
 }
