bba6ba9ce284ad9cdee1a9641e4ef65da4b6dfda
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-11-20 01:44:05 +0700

    Remove coba.php and add it to gitignore

--- coba.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use Nyetbot\Nyetbot;
-
-$nyetbot = new Nyetbot();
-$nyetbot->setChannelAcessToken("TDc5VrUTtzkRZXF9J77RkslmOLBxQiijbW38uW+j31oFyED8Znl9zQewUWNmsz6U/9uxeJ1RU3L47qJo7nbCfnUUaKp+Z4xrXzdYoJGBjoya6TCTrhYUp0El93JDlnrtcjmNVEOVtTxdGJrbpw98DgdB04t89/1O/w1cDnyilFU=");
-$nyetbot->setChannelSecret("497684041db169a6f0e4722226fe5321");
-$nyetbot->pushText("Ua3176d8484efdf60e0d71bad0ad7e9f4", "wekwek");
