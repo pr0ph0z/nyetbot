d650a885241018c654e0eb33cb9c7eaf5a49eca9
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-11-21 12:45:29 +0700

    Add method push image

--- src/Request/Message.php
+++ src/Request/Message.php
@@ -7,7 +7,8 @@ namespace Nyetbot\Request;
  * 
  * @package Request
  */
-class Message {
+class Message
+{
 
     public function __construct($parent)
     {
@@ -15,7 +16,7 @@ class Message {
     }
     
 	/**
-	 * Method to send a message without reply token (let's say it push).
+	 * Method to push a text message
 	 *
 	 * @param  mixed $to	Line user ID of the target
 	 * @param  mixed $text	The message
@@ -24,7 +25,8 @@ class Message {
 	 *
 	 * @return void
 	 */
-	public function pushText(string $to, string $text){
+    public function pushText(string $to, string $text): void
+    {
 		$this->bot->setApi("https://api.line.me/v2/bot/message/push");
 		$body = array(
 		    'to' => $to,
@@ -37,5 +39,32 @@ class Message {
 		);
 
 		$this->bot->http->post($body);
-	 }
+     }
+     
+     /**
+	 * Method to push a image message
+	 *
+	 * @param   string $to              Line user ID of the target
+	 * @param   string $imageUrl        URL of the image
+     * @param   string $previewImageUrl Preview URL of the image to be displayed
+     * @param mixed 
+	 * 
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
+     public function pushImage(string $to, string $imageUrl, string $previewImageUrl = null): void
+     {
+		$body = array(
+			'to' => $to,
+			'messages' => [
+				array(
+					'type' => 'image',
+					'originalContentUrl' => $imageUrl,
+					'previewImageUrl' => $previewImageUrl ? $previewImageUrl : $imageUrl
+				)
+			]
+        );
+        $this->bot->http->post($body);
+    }
 }
