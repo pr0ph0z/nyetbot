78e6315b67f19457d8b5091c2da4a6574537604f
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-11-21 10:35:18 +0700

    Move pushtext method to Message class

---
+++ src/Request/Message.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nyetbot\Request;
+
+/**
+ * Message class based on Request
+ * 
+ * @package Request
+ */
+class Message {
+    
+	/**
+	 * Method to send a message without reply token (let's say it push).
+	 *
+	 * @param  mixed $to	Line user ID of the target
+	 * @param  mixed $text	The message
+	 * 
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
+	public function pushText(string $to, string $text){
+		$this->setApi("https://api.line.me/v2/bot/message/push");
+		$body = array(
+		    'to' => $to,
+		    'messages' => [
+				array(
+					'type' => 'text',
+					'text' => $text
+				)
+		    ]
+		);
+
+		$this->post($body);
+	 }
+}
