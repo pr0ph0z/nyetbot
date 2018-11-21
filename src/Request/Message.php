1bfb394384fa7d43d2095373a4622fa45855e47a
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-11-21 12:45:44 +0700

    Add method push video

--- src/Request/Message.php
+++ src/Request/Message.php
@@ -67,4 +67,32 @@ class Message
         );
         $this->bot->http->post($body);
     }
+
+    /**
+	 * Method to push a video message
+	 *
+	 * @param   string $to              Line user ID of the target
+	 * @param   string $videoURL        URL of the video
+     * @param   string $previewImageUrl Preview URL of the image to be displayed
+     * @param mixed 
+	 * 
+	 * @since 0.0.1
+	 *
+	 * @return void
+	 */
+    public function pushVideo($to, $videoUrl, $previewImageUrl): void
+    {
+		$body = array(
+			'to' => $to,
+			'messages' => [
+				array(
+					'type' => 'video',
+					'originalContentUrl' => $videoUrl,
+					'previewImageUrl' => $previewImageUrl
+				)
+			]
+		);
+
+		$this->push($body);
+	}
 }
