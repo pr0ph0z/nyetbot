ccbeba380a228ed66c9bf857f37387d690a5c69b
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-12-06 14:10:28 +0700

    Add getMemberProfile method

--- src/Request/Room.php
+++ src/Request/Room.php
@@ -36,4 +36,21 @@ class Room {
 
         return $this->bot->http->get(array($roomId));
     }
+
+    /**
+     * Get member profile in room.
+     *
+     * @param  mixed $roomId
+     * @param  mixed $userId
+     * 
+     * @since 0.2.2
+     *
+     * @return string
+     */
+    public function getMemberProfile(string $roomId, string $userId): string
+    {
+        $this->bot->setApi(Http::ROOM_MEMBER_PROFILE_API);
+
+        return $this->bot->http->get(array($roomId, $userId));
+    }
 }
